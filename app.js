22dc5b6151e7af5cf4504500b02b3f92e1555d72
Leandro Giacomelli <leandroegiacomelli@gmail.com>
2023-06-17 02:56:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+const api = process.env.API_URL;
+const productsRouter = require("./routes/products");
+
+//middleware
+
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+app.use(`${api}/products`, productsRouter);
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("server is running http://localhost:3000");
+});
